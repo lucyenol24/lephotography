c2a485508ef893a3881173bb55c15b5ddb76bd48
lucyenol24 <lucynolan24@gmail.com>
2017-05-22 17:57:34 -0700

    put back old js

--- script.js
+++ script.js
@@ -1,49 +1,20 @@
-var location = [
-	{
-		"name": "Kirkland WA",
-		"lat": 47.6769,
-		"lng": -122.2060,
-		"zoom": 18
-	}];
+$('input, select, textarea').on('input change', valueEntered);
 
-var map;
-
-function initMap() {
-	map = new google.maps.Map($('#map')[0], {
-		center: {lat: 47.6205, lng: -122.3493},
-		zoom: 8
-	});
-
-	createMarkers();
-}
-
-function createMarkers() {
-	$.each(locations, function (index, value) {
-		var marker = new google.maps.Marker({
-        	position: { lat: value.lat, lng: value.lng }});
-		
-		marker.setMap(map);
-
-		var infoWindow = new google.maps.InfoWindow({
-        	content: value.name });
-	
-		marker.addListener( 'click', function( ) {
-        	infoWindow.open( map, marker );
-        });
-	});
-}
-
-$('#location').on('change', changeCenter);
-
-function changeCenter() {
-	var place = $(this).val();
-	var location = $.grep(locations, function (n, i) {
-		return n.name == place;
-	})[0];
-
-	if(location) {
-		map.setCenter({lat: location.lat, lng: location.lng});
-		map.setZoom(location.zoom);	
-	}
-	
-}
+function valueEntered() {
+  var element = $(this);
+  var row = element.closest("tr");
+  
+  if(element.is(':radio')) {
+    row.find('input').val([element.val()]);		row.find('.value').text(element.val());
+  }
+  else {
+    if(element.is(':checkbox')) {
+      row.find('input').prop('checked', element.prop('checked'));
+      row.find('.value').text(element.prop('checked'));
+    }
+    else {
+      row.find('input, select, textarea').val(element.val());
+      row.find('.value').text(element.val());
+    }
+  }
+} 
